160da0f3f1e7fa320c89c9fc05a0cfba85afa7a6
ca-trindade <piratum.core@gmail.com>
2024-01-04 17:12:02 +0000

    cleaning and fixing console errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,18 @@
 // "use client";
-// import axios from "axios";
 import Card from "@/components/card/Card";
-import getData from "../components/services/axios"
 import styles from "./page.module.css";
 import { Suspense } from "react";
 
 
-// const getData = async () => {
-//   const response = await axios.get(
-//     "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd"
-//   );
-//   return response.data;
-// };
-
-
-
 export default async function Home() {
-  // const response = await getData();
-  
-  // console.log(response)
+
   
   return <>
 
     <main className={styles.main}>
-      <Suspense fallback={<h3>Loading...</h3>}>
+      {<Suspense fallback={<h3>Loading...</h3>}>
         <Card />
-      </Suspense>
+      </Suspense>}
     </main>
   </>
 }
--- src/components/services/axios.tsx
+++ src/components/services/axios.tsx
@@ -1,10 +1,15 @@
 import axios from "axios";
 
 const getData = async () => {
-  const response = await axios.get(
-    "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd"
-  );
-  return response.data;
-};
-
+    try {
+        const response = await axios.get(
+            "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd"
+        );
+        return response.data;
+    }
+    catch (error) {
+        console.error("Erro ao obter dados da API:", error);
+        throw error;
+    }
+}
 export default getData;
